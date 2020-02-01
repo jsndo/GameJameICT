f058240ea040ce4490796e7ac10f49a01bc417bd
jdo <jason.html5@gmail.com>
2020-02-01 17:00:07 -0600

    Collision

--- GameJameICT/Entities/Mask.cs
+++ GameJameICT/Entities/Mask.cs
@@ -14,21 +14,32 @@ namespace GameJameICT.Entities
     public class Mask
     {
         private int numberOfMasks = 5;
+        private Texture2D texture;
+        
 
         //Masks will go down one point for every unit of time; adjust as needed
         private float _maskDepletionRate = 4.0f;
         private float _timeElapsed = 0.0f;
         private SpriteFont _font;
         private HealthBar _healthBar;
+        private Vector2 _position;
+        private Vector2 _dimensions;
 
         public void LoadContent(ContentManager content, HealthBar healthBar)
         {
             _font = content.Load<SpriteFont>("FontText");
+            texture = content.Load<Texture2D>("Mask");
+           
+
             _healthBar = healthBar;
+
+            _position = new Vector2(50, 50);
+            _dimensions = new Vector2(texture.Width, texture.Height);
         }
 
         public void Update(GameTime gameTime)
         {
+            //Console.WriteLine(_dimensions);
             if(numberOfMasks > 0)
             {
                 _healthBar.DepletionRate = 6.0f;
@@ -53,6 +64,23 @@ namespace GameJameICT.Entities
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.DrawString(_font, $"Masks Count: {numberOfMasks}", new Vector2(Screen.Width - 350, 50), Color.Green);
+            spriteBatch.Draw(texture, _position, Color.White);
+        }
+
+        public Vector2 Position
+        {
+            get { return _position; }
+        }
+
+        public Vector2 Dimensions
+        {
+            get { return _dimensions; }
+        }
+
+        public int IncrementCount
+        {
+            get { return numberOfMasks; }
+            set { numberOfMasks = value; }
         }
     }
 }
---
+++ GameJameICT/Entities/Player.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using GameJameICT.Common;
+using GameJameICT.Models;
+using GameJameICT.Sprites;
+
+namespace GameJameICT.Entities
+{
+    class Player
+    {
+        private List<Sprite> _sprites;
+        private Rectangle _rectangle;
+        public void LoadContent(ContentManager content)
+        {
+            
+            var animations = new Dictionary<string, Animation>()
+            {
+                {"WalkUp", new Animation(content.Load<Texture2D>("DrWalkingBack"),3)},
+                 {"WalkDown", new Animation(content.Load<Texture2D>("DrWalkingFront"),3)},
+                 {"WalkLeft", new Animation(content.Load<Texture2D>("DrWalkingLeft"),3)},
+                 {"WalkRight", new Animation(content.Load<Texture2D>("DrWalkingRight"),3)},
+
+            };
+            _sprites = new List<Sprite>()
+            {
+                new Sprite(animations)
+                {
+                    Position = new Vector2(100,100),
+                    Input = new Input()
+                    {
+                        Up = Keys.W,
+                        Down = Keys.S,
+                        Left = Keys.A,
+                        Right = Keys.D,
+
+                    },
+                },
+            };
+
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            foreach (var sprite in _sprites)
+                sprite.Update(gameTime, _sprites);
+
+
+            //Console.WriteLine(GetRect);
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (var sprite in _sprites)
+                sprite.Draw(spriteBatch);
+        }
+
+
+        public Vector2 Position
+        {
+            get { return _sprites.Last().Position; }
+        }
+
+        public Vector2 Dimensions
+        {
+            get { return _sprites.Last().Dimensions; }
+        }
+
+        public Rectangle GetRect
+        {
+            get { return _sprites.Last().GetRect; }
+        }
+    }
+}
